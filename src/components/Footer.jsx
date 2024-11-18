25fe19da93fb113fb78bcd500fa630d84f9bb70b
filipNajdovski <filip-najdovski@live.com>
2024-11-19 00:24:22 +0100

    updated footer will need future changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,19 @@
+import logo from '../assets/png/nedviznini-logo.png'
+import { useNavigate } from 'react-router-dom'
+
 function Footer() {
-  return (
-    <footer>
-        <div className="footerContainer">
-            <div className="footerLinks">
-                
+    const navigate = useNavigate()
+    return (
+        <footer>
+            <div className="footerContainer">
+                <div className="footerLogo" onClick={() => navigate('/')}>
+                    <img src={logo} alt="Nedvinini Logo" />
+                </div>
+                <p className="copyright">
+                    Website Developed by <a href="https://www.najdovski.tech/">Filip Najdovski</a>
+                </p>
             </div>
-            <p className="copyright">
-                Website Developed by <a href="https://www.najdovski.tech/">Filip Najdovski</a>
-            </p>
-        </div>
-    </footer>
-  )
+        </footer>
+    )
 }
 export default Footer
