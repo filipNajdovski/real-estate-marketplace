8116ffd9351911afc79aa131ffc1b014ee287ec8
filipNajdovski <filip-najdovski@live.com>
2024-11-19 00:03:10 +0100

    images for logo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer>
+        <div className="footerContainer">
+            <div className="footerLinks">
+                
+            </div>
+            <p className="copyright">
+                Website Developed by <a href="https://www.najdovski.tech/">Filip Najdovski</a>
+            </p>
+        </div>
+    </footer>
+  )
+}
+export default Footer
