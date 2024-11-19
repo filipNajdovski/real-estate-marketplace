04772b2509670b9ca1a3810f6e9278ba7bb8a6eb
filipNajdovski <filip-najdovski@live.com>
2024-11-19 11:38:34 +0100

    added copyright to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/png/nedviznini-logo.png'
+import filipLogo from '../assets/png/filip-logo1.png'
 import { useNavigate } from 'react-router-dom'
 
 function Footer() {
@@ -10,7 +11,7 @@ function Footer() {
                     <img src={logo} alt="Nedvinini Logo" />
                 </div>
                 <p className="copyright">
-                    Website Developed by <a href="https://www.najdovski.tech/">Filip Najdovski</a>
+                    Website Developed by <a href="https://www.najdovski.tech/"><img src={filipLogo} alt='Filip Najdovski' /> Filip Najdovski</a>
                 </p>
             </div>
         </footer>
